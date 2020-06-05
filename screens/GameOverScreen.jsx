c88444cef74d37d889af17ba919f5807d3cb89e5
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-05 16:51:24 -0600

    Adding margin on Title and Button

--- screens/GameOverScreen.jsx
+++ screens/GameOverScreen.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, StyleSheet, Image, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
 import Colors from "../constants/colors";
@@ -7,32 +14,36 @@ import MainButton from "../components/MainButton";
 
 const GameOverScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("../assets/success.png")}
-          // source={{
-          //   uri:
-          //     "https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg",
-          // }}
-          style={styles.image}
-          resizeMode="cover"
-          fadeDuration={1000}
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText style={styles.title}>The Game is over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/success.png")}
+            // source={{
+            //   uri:
+            //     "https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg",
+            // }}
+            style={styles.image}
+            resizeMode="cover"
+            fadeDuration={1000}
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed{" "}
+            <Text Text style={styles.highlight}>
+              {props.rounds}
+            </Text>{" "}
+            rounds to guess the number{" "}
+            <Text style={styles.highlight}>{props.userNumber}</Text>.
+          </BodyText>
+        </View>
+        <MainButton style={styles.mainButton} onPress={props.onRestart}>
+          NEW GAME
+        </MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed{" "}
-          <Text Text style={styles.highlight}>
-            {props.rounds}
-          </Text>{" "}
-          rounds to guess the number{" "}
-          <Text style={styles.highlight}>{props.userNumber}</Text>.
-        </BodyText>
-      </View>
-      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -42,14 +53,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  title: {
+    marginVertical: Dimensions.get("window").height / 40,
+  },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
+    borderRadius: (Dimensions.get("window").width * 0.7) / 2, // here always have to be half of the width and height of the container so we have a perfect circle
     borderWidth: 3,
     borderColor: "black",
     overflow: "hidden", // any child inside of the container that go beyond the boundaries will be clipped (or cut off)
-    marginVertical: 30,
+    marginVertical: Dimensions.get("window").height / 40,
   },
   image: {
     width: "100%",
@@ -61,11 +75,14 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 15,
+    marginVertical: Dimensions.get("window").height / 80,
   },
   resultText: {
     textAlign: "center",
-    fontSize: 20,
+    fontSize: Dimensions.get("window").height < 400 ? 16 : 20,
+  },
+  mainButton: {
+    marginVertical: 10,
   },
 });
 
