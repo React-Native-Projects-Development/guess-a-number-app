1c0f224577bfc7f40916ef839e2905ba17247f5e
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-05 15:29:38 -0600

    Using dimensions to assing width dinamically in 'if' checks

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -4,8 +4,8 @@ import {
   Text,
   StyleSheet,
   Alert,
-  ScrollView,
   FlatList,
+  Dimensions,
 } from "react-native";
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
@@ -79,6 +79,12 @@ const GameScreen = (props) => {
     ]);
   };
 
+  let listContainerStyle = styles.listContainer;
+
+  if (Dimensions.get("window").width < 350) {
+    listContainerStyle = styles.listContainerBig;
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={DefaultStyles.title}>Opponent's Guess</Text>
@@ -91,7 +97,7 @@ const GameScreen = (props) => {
           <Ionicons name="md-add" size={24} color="white" />
         </MainButton>
       </Card>
-      <View style={styles.listContainer}>
+      <View style={listContainerStyle}>
         {/* <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) => {
             // console.log("Guess", guess, "Index", index);
@@ -119,13 +125,17 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%",
   },
   listContainer: {
     flex: 1, // for Android is necessary to add this property
     width: "60%",
+  },
+  listContainerBig: {
+    flex: 1,
+    width: "80%",
   },
   list: {
     flexGrow: 1, // is more flexible than flex, allows the item to take as much space as it can and keeps the rest of the behaviour exactly as it is (mostly for ScrollView)
