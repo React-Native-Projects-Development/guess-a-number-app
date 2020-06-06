c5d2eb2f62635955dfe8086a0c0f7427a9cfe5d1
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-05 18:33:42 -0600

    Showing specific button styles based on platform

--- components/MainButton.jsx
+++ components/MainButton.jsx
@@ -1,19 +1,39 @@
 import React from "react";
-import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from "react-native";
 
 import Colors from "../constants/colors";
 
 const MainButton = (props) => {
+  let ButtonComponent = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    ButtonComponent = TouchableNativeFeedback;
+  }
+
   return (
-    <TouchableOpacity onPress={props.onPress} activeOpacity={0.6}>
-      <View style={{ ...styles.button, ...props.style }}>
-        <Text style={styles.buttonText}>{props.children}</Text>
-      </View>
-    </TouchableOpacity>
+    <View style={styles.buttonContainer}>
+      <ButtonComponent onPress={props.onPress} activeOpacity={0.6}>
+        <View style={{ ...styles.button, ...props.style }}>
+          <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+      </ButtonComponent>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 25,
+    overflow: "hidden",
+  },
   button: {
     backgroundColor: Colors.primary,
     paddingVertical: 12,
